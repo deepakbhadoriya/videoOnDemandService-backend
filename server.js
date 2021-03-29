4ef4f29f7fd9893763b40b2afbb2a977d89bbbac
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2021-03-29 14:44:42 +0530

    disabled frontend deploy

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ require('./startup/db')();
 require('./startup/routes')(app);
 
 //Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  //Set Static Folder
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   //Set Static Folder
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => winston.info(`Server started at port ${PORT}`));
